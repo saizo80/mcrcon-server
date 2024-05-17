df5592fa24db9ad6053caae33cadf095be8b7c35
sai <oli@smail.is>
2024-05-17 16:09:04 -0500

    sql interface

--- mcSql/mcSql.go
+++ mcSql/mcSql.go
@@ -2,15 +2,31 @@ package mcSql
 
 import (
 	"database/sql"
+	"fmt"
 
 	_ "github.com/mattn/go-sqlite3"
+	"github.com/saizo80/mcrcon-server/log"
+)
+
+const (
+	databaseFile = "./database.db"
 )
 
 type Player struct {
-	UUID     string
-	Name     string
-	LastSeen string
-	Active   bool
+	UUID        string
+	Name        string
+	LastSeen    string
+	Active      bool
+	Op          bool
+	Banned      bool
+	Whitelisted bool
+}
+
+func (p Player) String() string {
+	return fmt.Sprintf(
+		"UUID: %s, Name: %s, LastSeen: %s, Active: %t, Op: %t, Banned: %t, Whitelisted: %t",
+		p.UUID, p.Name, p.LastSeen, p.Active, p.Op, p.Banned, p.Whitelisted,
+	)
 }
 
 type SQLInterface struct {
@@ -18,29 +34,33 @@ type SQLInterface struct {
 	DB *sql.DB
 }
 
-func (s *SQLInterface) Init() SQLInterface {
+func (s *SQLInterface) Init() error {
+	log.Debug("opening database connection")
 	// connect to the database
-	db, err := sql.Open("sqlite3", "./database.db")
+	db, err := sql.Open("sqlite3", databaseFile)
 	if err != nil {
-		panic(err)
+		return log.Error(err)
 	}
 	s.DB = db
-	s.setup()
-	return *s
+	return s.setup()
 }
 
-func (s *SQLInterface) setup() {
+func (s *SQLInterface) setup() error {
 	// create the players table
-	s.DB.Exec(`create table if not exists players (
+	log.Debug("creating players table")
+	_, err := s.DB.Exec(`create table if not exists players (
 		uuid text primary key,
 		name text,
 		last_seen text,
-		active boolean
+		active boolean,
+		op boolean,
+		banned boolean,
+		whitelisted boolean
 	)`)
-
+	return log.Error(err)
 }
 
-func (s *SQLInterface) GetPlayers() []Player {
+func (s *SQLInterface) GetPlayers() ([]Player, error) {
 	rows := s.Query("select * from players")
 	players := []Player{}
 	for rows.Next() {
@@ -48,10 +68,16 @@ func (s *SQLInterface) GetPlayers() []Player {
 		var name string
 		var last_seen string
 		var active bool
-		rows.Scan(&uuid, &name, &last_seen, &active)
-		players = append(players, Player{uuid, name, last_seen, active})
+		var op bool
+		var banned bool
+		var whitelisted bool
+		err := rows.Scan(&uuid, &name, &last_seen, &active, &op, &banned, &whitelisted)
+		if err != nil {
+			return nil, log.Error(err)
+		}
+		players = append(players, Player{uuid, name, last_seen, active, op, banned, whitelisted})
 	}
-	return players
+	return players, nil
 }
 
 func (s *SQLInterface) InsertPlayer(player Player) {
@@ -59,16 +85,52 @@ func (s *SQLInterface) InsertPlayer(player Player) {
 	if err != nil {
 		panic(err)
 	}
-	tx.Exec("insert into players (uuid, name, last_seen, active) values (?, ?, ?, ?)", player.UUID, player.Name, player.LastSeen, player.Active)
+	exec := `insert or ignore into players (
+		uuid, name, last_seen, active, op, banned, whitelisted
+	) values (?, ?, ?, ?, ?, ?, ?)`
+	tx.Exec(exec, player.UUID, player.Name, player.LastSeen, player.Active, player.Op, player.Banned, player.Whitelisted)
 	tx.Commit()
 }
 
-func (s *SQLInterface) UpdatePlayerActive(uuid string, active bool) {
+func (s *SQLInterface) UpdatePlayerActive(uuid string, active bool) error {
+	tx, err := s.DB.Begin()
+	if err != nil {
+		tx.Rollback()
+		return log.Error(err)
+	}
+	_, err = tx.Exec("update players set active = ? where (uuid = ? or name = ?)", active, uuid, uuid)
+	if err != nil {
+		tx.Rollback()
+		return log.Error(err)
+	}
+	tx.Commit()
+	return nil
+}
+
+func (s *SQLInterface) UpdatePlayerOp(uuid string, op bool) {
 	tx, err := s.DB.Begin()
 	if err != nil {
 		panic(err)
 	}
-	tx.Exec("update players set active = ? where uuid = ?", active, uuid)
+	tx.Exec("update players set op = ? where uuid = ?", op, uuid)
+	tx.Commit()
+}
+
+func (s *SQLInterface) UpdatePlayerBanned(uuid string, banned bool) {
+	tx, err := s.DB.Begin()
+	if err != nil {
+		panic(err)
+	}
+	tx.Exec("update players set banned = ? where uuid = ?", banned, uuid)
+	tx.Commit()
+}
+
+func (s *SQLInterface) UpdatePlayerWhitelisted(uuid string, whitelisted bool) {
+	tx, err := s.DB.Begin()
+	if err != nil {
+		panic(err)
+	}
+	tx.Exec("update players set whitelisted = ? where uuid = ?", whitelisted, uuid)
 	tx.Commit()
 }
 
@@ -78,4 +140,18 @@ func (s *SQLInterface) Query(query string) *sql.Rows {
 		panic(err)
 	}
 	return rows
+}
+
+func (s *SQLInterface) Exec(stmt string, args ...interface{}) (sql.Result, error) {
+	tx, err := s.DB.Begin()
+	if err != nil {
+		return nil, err
+	}
+	result, err := tx.Exec(stmt, args...)
+	if err != nil {
+		tx.Rollback()
+		return nil, err
+	}
+	tx.Commit()
+	return result, nil
 }
